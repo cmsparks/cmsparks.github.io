aa3b25ebf230a497abc3f43405214089b1fe3e53
cmsparks <Christian@Christians-MacBook-Air.local>
2017-10-24 15:03:08 -0500

    added http test

--- test.js
+++ test.js
@@ -1,10 +1,13 @@
 console.log("loaded test");
-function httpGet(theUrl)
+function httpGetAsync(theUrl, callback)
 {
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
 }
 console.log(httpGet("https://www.ctabustracker.com/bustime/api/v2/gettime?key=tZWNpjTrjnM5rMh8xLpeM8X95"));
 console.log("finished http get");
